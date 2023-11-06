62e903f6e96d1ad27498db5b12d894c18d4ef795
alexander <alexandernyaga1999@gmail.com>
2023-11-06 06:39:40 +0300

    Completed assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,7 +2,37 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 function NavBar() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <div>
+    <nav>
+        <ul>
+          <li>
+            <NavLink to="/" exact activeClassName="active">
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/movies" activeClassName="active">
+              Movies
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/directors" activeClassName="active">
+
+            Directors
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/actors" activeClassName="active">
+              Actors
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
+
+  
 }
 
 export default NavBar;
